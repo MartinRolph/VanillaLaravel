7c64f58560240e68c0b5b559bec614cfa38db3b5
ovaljamestrusler <james.trusler@oval.uk.com>
2015-02-24 12:06:48 +0000

    - Remove this Kernel addition as been told it does not need to be there

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,8 +11,7 @@ class Kernel extends ConsoleKernel {
 	 * @var array
 	 */
 	protected $commands = [
-            'App\Console\Commands\Inspire',
-            'Paolooo\LaravelDoctrine\Console\DoctrineCommand'
+            'App\Console\Commands\Inspire'
 	];
 
 	/**
